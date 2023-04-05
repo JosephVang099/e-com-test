c95754063695f8f53f65b5ada8ffe95c40226067
JosephVang099 <joseph-vang@outlook.com>
2023-04-05 15:56:38 -0400

    Update shop.jsx

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,39 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
+
 import "./shop.css";
-
-const Container = styled.div`
-
-`;
-const Title = styled.h1`
-    margin-top: 100px;
-    text-align: center;
-    font-size: 40px;
-`;
-const Products = styled.div`
-    width: 100%;
-    height: auto;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    place-items: center;
-`;
 
 
 
 export const Shop = () => {
   return (
-    <Container className="shop">
-      <Title className="shopTitle">
-        AZN BUZZIN SHOP
-      </Title>
-
-      <Products className="products">
+    <div className="shop">
+        <div className="products">
         {PRODUCTS.map((product) => (
-          <Product data={product} />
+            <Product data={product} />
         ))}
-      </Products>
-    </Container>
-  );
+        </div>
+  </div>
+);
 };
