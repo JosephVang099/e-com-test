800a40e4c636ab1469736c74254f27db450c9752
JosephVang099 <joseph-vang@outlook.com>
2023-04-05 12:51:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import Newsletter from "./components/Newsletter";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        < Navbar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/shop" />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
+      </Router>
+
     </div>
+        
+        
   );
 }
 
