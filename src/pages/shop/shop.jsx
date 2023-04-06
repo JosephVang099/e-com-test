5ba9daa9256831efb7554eb9ca4111a925ca1ea2
JosephVang099 <joseph-vang@outlook.com>
2023-04-06 11:50:43 -0400

    Update shop.jsx

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Filter from "../../components/Filter";
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
 
@@ -8,12 +9,15 @@ import "./shop.css";
 
 export const Shop = () => {
   return (
+    <div>
+    <Filter />
     <div className="shop">
         <div className="products">
         {PRODUCTS.map((product) => (
             <Product data={product} />
         ))}
         </div>
+  </div>
   </div>
 );
 };
